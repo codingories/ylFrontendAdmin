15824087084c9cd26988bc56c4fce4e4383796d2
Ories <tellt@126.com>
2022-08-19 13:18:16 +0800

    8-2 菜单新增实现（上）完成

--- src/views/Menu.vue
+++ src/views/Menu.vue
@@ -21,7 +21,7 @@
     </div>
     <div class="base-table">
       <div class="action">
-        <el-button type="primary" @click="handleAdd">新增</el-button>
+        <el-button type="primary" @click="handleAdd(1)">新增</el-button>
       </div>
       <el-table
           :data="menuList"
@@ -41,71 +41,63 @@
             label="操作"
             width="220">
           <template #default="scope">
-            <el-button type="primary" @click="handleAdd(scope.row)">新增</el-button>
+            <el-button type="primary" @click="handleAdd(2, scope.row)">新增</el-button>
             <el-button type="primary" @click="handleEdit(scope.row)">编辑</el-button>
             <el-button type="danger" size="small" @click="handleDel(scope.row)">删除</el-button>
           </template>
         </el-table-column>
       </el-table>
     </div>
-    <!--    <el-dialog v-model="showModal" title="用户新增">-->
-    <!--      <el-form ref="dialogForm" :model="userForm" :label-width="100" :rules="rules">-->
-    <!--        <el-form-item label="用户名" prop="userName">-->
-    <!--          <el-input v-model="userForm.userName" placeholder="请输入用户名称"-->
-    <!--                    :disabled="action === 'edit'"-->
-    <!--          />-->
-    <!--        </el-form-item>-->
-    <!--        <el-form-item label="邮箱" prop="userEmail">-->
-    <!--          <el-input v-model="userForm.userEmail" placeholder="请输入用户邮箱"-->
-    <!--                    :disabled="action === 'edit'"-->
-    <!--          >-->
-    <!--            <template #append>-->
-    <!--              @myCompany.com-->
-    <!--            </template>-->
-    <!--          </el-input>-->
-    <!--        </el-form-item>-->
-    <!--        <el-form-item label="手机号" prop="mobile">-->
-    <!--          <el-input v-model="userForm.mobile" placeholder="请输入用户手机号"/>-->
-    <!--        </el-form-item>-->
-    <!--        <el-form-item label="岗位" prop="job">-->
-    <!--          <el-input v-model="userForm.job" placeholder="请输入用户岗位"/>-->
-    <!--        </el-form-item>-->
-    <!--        <el-form-item label="状态" prop="state">-->
-    <!--          <el-select v-model="userForm.state" placeholder="请输入用户状态">-->
-    <!--            <el-option :value="1" label="在职"></el-option>-->
-    <!--            <el-option :value="2" label="离职"></el-option>-->
-    <!--            <el-option :value="3" label="试用期"></el-option>-->
-    <!--          </el-select>-->
-    <!--        </el-form-item>-->
-    <!--        <el-form-item label="系统角色 " prop="roleList">-->
-    <!--          <el-select v-model="userForm.roleList" placeholder="请输入用户系统角色" multiple style="width: 100%">-->
-    <!--            <el-option-->
-    <!--                v-for="role in roleList"-->
-    <!--                :key="role._id"-->
-    <!--                :label="role.roleName"-->
-    <!--                :value="role._id"-->
-    <!--            ></el-option>-->
-    <!--          </el-select>-->
-    <!--        </el-form-item>-->
-    <!--        <el-form-item label="部门" prop="deptId">-->
-    <!--          &lt;!&ndash; value: '_id', label: 'deptName'  可以指定空间用的value 和label 对应接口的哪个字段 &ndash;&gt;-->
-    <!--          <el-cascader-->
-    <!--              v-model="userForm.deptId"-->
-    <!--              placeholder="请选择所属部门"-->
-    <!--              :options="deptList"-->
-    <!--              :props="{ checkStrictly: true, value: '_id', label: 'deptName', expandTrigger: 'click' }"-->
-    <!--              clearable-->
-    <!--              style="width: 100%"-->
-    <!--          ></el-cascader>-->
-    <!--        </el-form-item>-->
-    <!--      </el-form>-->
-    <!--      <template #footer>-->
-    <!--      <span class="dialog-footer">-->
-    <!--          <el-button @click="handleClose">取 消</el-button>-->
-    <!--          <el-button type="primary" @click="handleSubmit">确 定</el-button>-->
-    <!--      </span>-->
-    <!--      </template>-->
-    <!--    </el-dialog>-->
+    <el-dialog v-model="showModal" title="用户新增">
+      <el-form ref="dialogForm" :model="menuForm" :label-width="100" :rules="rules">
+        <el-form-item label="父级菜单" prop="parentId">
+          <el-cascader
+              v-model="menuForm.parentId"
+              :options="menuList"
+              :props="{ checkStrictly: true }"
+              clearable
+          >
+
+          </el-cascader>
+          <span>
+            不选，则直接创建一级菜单
+          </span>
+        </el-form-item>
+        <el-form-item label="菜单类型" prop="menuType">
+          <el-radio-group v-model="menuForm.menuType">
+            <el-radio label="1">菜单</el-radio>
+            <el-radio label="2">按钮</el-radio>
+          </el-radio-group>
+        </el-form-item>
+        <el-form-item label="菜单名称" prop="menuName">
+          <el-input v-model="menuForm.menuName" placeholder="请输入菜单名称"/>
+        </el-form-item>
+        <el-form-item label="菜单图标" prop="icon">
+          <el-input v-model="menuForm.icon" placeholder="请输入图标"/>
+        </el-form-item>
+        <el-form-item label="路由地址" prop="path">
+          <el-input v-model="menuForm.path" placeholder="请输入路由地址"/>
+        </el-form-item>
+        <el-form-item label="权限标识" prop="menuCode">
+          <el-input v-model="menuForm.menuCode" placeholder="请输入权限标识"/>
+        </el-form-item>
+        <el-form-item label="组件路径" prop="component">
+          <el-input v-model="menuForm.component" placeholder="请输入组件路径"/>
+        </el-form-item>
+        <el-form-item label="菜单状态" prop="status">
+          <el-radio-group v-model="menuForm.status">
+            <el-radio label="1">正常</el-radio>
+            <el-radio label="2">停用</el-radio>
+          </el-radio-group>
+        </el-form-item>
+      </el-form>
+      <template #footer>
+      <span class="dialog-footer">
+          <el-button @click="handleClose">取 消</el-button>
+          <el-button type="primary" @click="handleSubmit">确 定</el-button>
+      </span>
+      </template>
+    </el-dialog>
   </div>
 </template>
 
@@ -116,10 +108,12 @@ export default {
   name: 'menu',
   data() {
     return {
+      menuForm: {},
+      showModal: true,
       queryForm: {
         menuState: 1
       },
-
+      action: '',
       menuList: [],
       columns: [
         {
@@ -195,6 +189,11 @@ export default {
     handleReset() {
     },
     handleAdd() {
+      this.showModal = true
+      this.action = "add"
+      if (type === 2) {
+        this.menuForm.parentId = [...row.parentId, row._id].filter((item) => item)
+      }
     },
     handleEdit() {
     },
