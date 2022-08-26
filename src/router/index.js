b895113e8e6a9226f7c17811bdbb5adb3123402d
Ories <tellt@126.com>
2022-08-26 14:42:51 +0800

    11-5 404页面开发完成

--- src/router/index.js
+++ src/router/index.js
@@ -62,6 +62,14 @@ const routes = [
     },
     component: () => import('@/views/Login.vue'),
   },
+  {
+    name: '404',
+    path: '/404',
+    meta: {
+      title: '页面不存在'
+    },
+    component: () => import('@/views/404.vue'),
+  },
 
 ]
 
