cdeaa7491f2b700e33b675c3158cc7e4fef48d24
Ories <tellt@126.com>
2022-08-31 10:32:28 +0800

    14-8 v-model实现完成

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,27 @@
 <template>
   <!--  会渲染组件到里面,默认的根组件-->
-  <router-view></router-view>
+<!--  <router-view></router-view>-->
+  <v-input v-model="userName" v-model:title="title" @update:handleSubmit="handleSubmit"></v-input>
 </template>
 <script>
+import VInput from "./views/VInput.vue"
+import {ref} from "vue"
+
 export default {
   name: 'app',
-  mounted() {
-    // this.$storage.setItem("name", "Ories")
-    // console.log('name', this.$storage.getItem("name"))
-    // this.$storage.setItem("age", 30)
-    // console.log('age', this.$storage.getItem("age"))
-    // this.$storage.setItem("userInfo", {age: 22, name: "Jorden"})
-    // console.log('userInfo', this.$storage.getItem("userInfo"))
-    // this.$storage.clearItem("age")
-    // this.$storage.clearAll()
+  components: {VInput},
+  setup() {
+    let userName = ref('jack')
+    let title = ref('Vue3')
+    const handleSubmit = (values) => {
+      console.log("接受数据", values)
+      console.log("当前对象:", userName)
+    }
+    return {
+      userName,
+      title,
+      handleSubmit
+    }
   }
 }
 </script>
